448c2c5988c3c82e26a34025396160457e267829
jayanta <jayanta.m@digitaledge360.in>
2024-12-16 17:20:20 +0530

    multi user try

--- index.js
+++ index.js
@@ -1,119 +1,3 @@
-// const express = require('express');
-// var cors = require('cors');
-// const app = express();
-// const webrtc = require("wrtc");
-
-// let senderStream;
-
-// app.use(express.static('public'));
-// app.use(express.json());
-// app.use(cors())
-// app.get("/", (req, res) => {
-//     res.sendFile(__dirname + "/index.html");
-// });
-// app.post("/consumer", async (req, res) => {
-//     try {
-//         const sdp = req.body.offer;
-//         console.log("CONSUMER\n::::::::::::::::::::::::::::::::\n");
-//         const offer = {
-//             sdp: sdp,
-//             type: "offer",
-//         }
-//         const peer = new webrtc.RTCPeerConnection({
-//             'iceServers': [
-//                 {
-//                     'urls': [
-//                         'stun:stun1.l.google.com:19302',
-//                         'stun:stun2.l.google.com:19302'
-//                     ]
-//                 },
-//                 {
-//                     'urls': [
-//                         'turn:3.111.40.187:3478',  // TURN over UDP
-//                         'turns:3.111.40.187:5349' // TURN over TLS
-//                     ],
-//                     'username': 'jiturn',
-//                     'credential': 'jiturnpass',
-//                 }
-
-//             ],
-//             iceTransportPolicy: "relay",
-//         });
-//         peer.onicecandidate = (event) => {
-//             if (event.candidate) {
-//                 console.log("New ICE Candidate:", event.candidate.candidate);
-//             }
-//         };
-//         const desc = new webrtc.RTCSessionDescription(offer);
-//         await peer.setRemoteDescription(desc);
-//         senderStream.getTracks().forEach(track => peer.addTrack(track, senderStream));
-//         const answer = await peer.createAnswer();
-//         await peer.setLocalDescription(answer);
-
-
-//         res.json(peer.localDescription);
-
-//     } catch (error) {
-//         res.status(500).send(error.message);
-//     }
-// });
-
-// app.post('/broadcast', async (req, res) => {
-//     try {
-
-//         const sdp = req.body.offer;
-//         console.log("ASTROLOGER\n::::::::::::::::::::::::::::::::\n",);
-//         const offer = {
-//             sdp: sdp,
-//             type: "offer",
-//         }
-
-//         const peer = new webrtc.RTCPeerConnection({
-//             'iceServers': [
-//                 {
-//                     'urls': [
-//                         'stun:stun1.l.google.com:19302',
-//                         'stun:stun2.l.google.com:19302'
-//                     ]
-//                 },
-//                 {
-//                     'urls': [
-//                         'turn:3.111.40.187:3478',  // TURN over UDP
-//                         'turns:3.111.40.187:5349' // TURN over TLS
-//                     ],
-//                     'username': 'jiturn',
-//                     'credential': 'jiturnpass',
-//                 }
-//             ],
-//             iceTransportPolicy: "relay",
-//         });
-//         peer.ontrack = (e) => handleTrackEvent(e, peer);
-//         peer.onicecandidate = (event) => {
-//             if (event.candidate) {
-//                 console.log("New ICE Candidate:", event.candidate.candidate);
-//             }
-//         };
-//         const desc = new webrtc.RTCSessionDescription(offer);
-//         await peer.setRemoteDescription(desc);
-//         const answer = await peer.createAnswer();
-//         await peer.setLocalDescription(answer);
-//         res.json(peer.localDescription);
-//     } catch (error) {
-//         console.log(error);
-//         console.log("*******************************");
-//         console.log(error.message);
-//         res.status(500).json({ error: error.message });
-
-//     }
-// });
-
-// function handleTrackEvent(e, peer) {
-//     senderStream = e.streams[0];
-// };
-
-
-// app.listen(5000, () => console.log('server started on 5000'));
-
 
 const express = require('express');
 var cors = require('cors');
@@ -140,9 +24,8 @@ app.post("/consumer", async (req, res) => {
             return res.status(404).json({ error: "Broadcast not found" });
         }
 
-        console.log("---------------------------\n", broadcasts);
 
-        const senderStream = broadcasts[broadcastId];
+        const senderStream = broadcasts[broadcastId].stream;
 
         const peer = new webrtc.RTCPeerConnection({
             iceServers: [
@@ -169,10 +52,11 @@ app.post("/consumer", async (req, res) => {
                 console.log("New ICE Candidate:", event.candidate.candidate);
             }
         };
+        senderStream.getTracks().forEach((track) => peer.addTrack(track, senderStream));
 
         const desc = new webrtc.RTCSessionDescription({ sdp: offer, type: "offer" });
         await peer.setRemoteDescription(desc);
-        senderStream.getTracks().forEach(track => peer.addTrack(track, senderStream));
+        // senderStream.getTracks().forEach(track => peer.addTrack(track, senderStream));
 
         const answer = await peer.createAnswer();
         await peer.setLocalDescription(answer);
@@ -207,11 +91,11 @@ app.post('/broadcast', async (req, res) => {
             ],
             iceTransportPolicy: "all",
         });
-        peer.ontrack = (e) => {
-            // Save broadcaster's stream
-            broadcasts[broadcastId] = e.streams[0];
-
-
+        peer.ontrack = (event) => {
+            broadcasts[broadcastId] = {
+                peer,
+                stream: event.streams[0]
+            };
         };
         peer.onicecandidate = (event) => {
             if (event.candidate) {
